9ca18a38cfb73d39f3082a406904a2dc052ebc46
evan-poster <evan.poster@gmail.com>
2025-09-17 21:26:30 -0400

    Initial Commit

---
+++ CarDealer/CarDealerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarDealerApp.swift
+//  CarDealer
+//
+//  Created by Evan Poster on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct CarDealerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarDealerTests/CarDealerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarDealerTests.swift
+//  CarDealerTests
+//
+//  Created by Evan Poster on 9/17/25.
+//
+
+import Testing
+@testable import CarDealer
+
+struct CarDealerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
